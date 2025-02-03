bc4faf752a907f74db98e192fee8ae0b795c4a24
parth_b <borad0parth004@gmail.com>
2025-02-03 23:37:29 +0530

    change properties

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,23 @@
  
   const nodes = writable([
     {
-      id: '1', // required and needs to be a string
-      position: { x: 0, y: 0 }, // required
-      data: { label: 'hey' } // required
+      id: '1',
+      position: { x: 0, y: 0 },
+      data: { label: 'A' }
     },
     {
       id: '2',
       position: { x: 100, y: 100 },
-      data: { label: 'world' }
+      data: { label: 'B' }
     }
   ]);
  
-  const edges = writable([]);
+  const edges = writable([{ id: '1-2', source: '1', target: '2' }]);
 </script>
  
 <main>
   <SvelteFlow {nodes} {edges}>
-    <Background />
+    <Background bgColor="rgba(255,255,255,0.25)" patternColor="red" />
     <Controls />
   </SvelteFlow>
 </main>
